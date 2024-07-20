6c00a24eeffb26a24977a5f409bb0a102b0c23c9
Rmjp <rmjp15.rmt@gmail.com>
2024-07-20 09:01:34 +0700

    fix: getHello to call Kafka and handle response asynchronously

--- gateway/src/app.controller.ts
+++ gateway/src/app.controller.ts
@@ -3,16 +3,18 @@ import { AppService } from './app.service';
 import { ClientKafka } from '@nestjs/microservices';
 import { Admin } from '@nestjs/microservices/external/kafka.interface';
 import { Kafka } from 'kafkajs';
+import { lastValueFrom, Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
-  constructor(@Inject('KAFKA') private client: ClientKafka) {}
+  constructor(@Inject('KAFKA') private client: ClientKafka, private appService: AppService) {}
   private admin: Admin;
-  private readonly appService: AppService;
 
   @Get('/hello')
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    const result = await this.client.send('hello', 'Hello Kafka!');
+    const value = await lastValueFrom(result);
+    return value
   }
 
   async onModuleInit() {
