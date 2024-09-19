62278425dc61e71246e777dada5a2fe99b08afdf
Rmjp <rmjp15.rmt@gmail.com>
2024-09-19 23:43:52 +0700

    payment

--- services/requester/src/payment/payment.module.ts
+++ services/requester/src/payment/payment.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CacheModule.register()],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
--- services/requester/src/payment/payment.service.ts
+++ services/requester/src/payment/payment.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+
+import { PaymentToken } from './dto/payment_token';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+    constructor(private readonly httpService: HttpService, @Inject(CACHE_MANAGER) private cacheManager: Cache){}
+
+    async getToken(){
+        const json_data = await (await this.cacheManager.match('PAYMENT_TOKEN')).json();
+        let payment_token = new PaymentToken();
+        if(!json_data){
+            this.httpService.post('https://api-sandbox.partners.scb/partners/sandbox/v1/oauth/token', {
+                "applicationKey" : "l73c276a43d10949f9a3f6ec564a63926c",
+                "applicationSecret" : "7ee5a48b9bf549a0a4d64078e39cacb9"
+            },
+        )
+        }
+    }
+}
