44b4ce193e017cf3d932424f611a092e4b02d22b
Rmjp <rmjp15.rmt@gmail.com>
2024-09-19 21:24:53 +0700

    payment init

--- services/requester/src/app.module.ts
+++ services/requester/src/app.module.ts
@@ -8,9 +8,10 @@ import { PrismaModule } from './prisma.module';
 import { OrderModule } from './order/order.module';
 import { ReportModule } from './report/report.module';
 import { AddressModule } from './address/address.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [CanteenModule, CanteenModule, ProfileModule, AuthModule, PrismaModule, OrderModule, ReportModule, AddressModule],
+  imports: [CanteenModule, CanteenModule, ProfileModule, AuthModule, PrismaModule, OrderModule, ReportModule, AddressModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
