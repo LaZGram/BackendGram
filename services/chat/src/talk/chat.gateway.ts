0d8bee1d6a891e3e8baf298e4e4aef219273e139
Setthawut Apichai <99123775+LaZGram@users.noreply.github.com>
2024-10-04 10:41:22 +0700

    Update chat.gateway.ts

--- services/chat/src/talk/chat.gateway.ts
+++ services/chat/src/talk/chat.gateway.ts
@@ -8,7 +8,7 @@ import {
   import { Server, Socket } from 'socket.io';
   import { ChatService } from './chat.service';
   
-  @WebSocketGateway()
+  @WebSocketGateway({ cors: true })
   export class ChatGateway {
     @WebSocketServer()
     server: Server;
