60271b98f243b56c0396e33c651e7d06a8f0fdf4
Rmjp <rmjp15.rmt@gmail.com>
2024-10-18 00:08:48 +0700

    Refactor auth.module.ts to include ConfigModule for global environment configuration

--- services/requester/src/auth/auth.module.ts
+++ services/requester/src/auth/auth.module.ts
@@ -3,9 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+    envFilePath: `.env`,
+    isGlobal: true,
+  }),
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
